690eca92a9d39c23b1f647923e60235a87bfb9d9
Giacomo Baso <gbaso@users.noreply.github.com>
2025-03-17 17:33:14 +0100

    add application

---
+++ src/main/java/com/github/gbaso/grpc/csrf/SpringGrpcClientSecurityCsrfApplication.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.grpc.csrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGrpcClientSecurityCsrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGrpcClientSecurityCsrfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gbaso/grpc/csrf/SpringGrpcClientSecurityCsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.grpc.csrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGrpcClientSecurityCsrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
