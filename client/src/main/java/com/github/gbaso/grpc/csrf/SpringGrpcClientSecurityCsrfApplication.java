3b390ee654962ca5202cfbe126cf9cc86c506293
Giacomo Baso <gbaso@users.noreply.github.com>
2025-03-17 18:45:50 +0100

    configure client application

--- client/src/main/java/com/github/gbaso/grpc/csrf/SpringGrpcClientSecurityCsrfApplication.java
+++ client/src/main/java/com/github/gbaso/grpc/csrf/SpringGrpcClientSecurityCsrfApplication.java
@@ -1,7 +1,18 @@
 package com.github.gbaso.grpc.csrf;
 
+import com.github.gbaso.grpc.hello.SimpleGrpc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.grpc.client.GrpcChannelFactory;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
 public class SpringGrpcClientSecurityCsrfApplication {
@@ -10,4 +21,26 @@ public class SpringGrpcClientSecurityCsrfApplication {
 		SpringApplication.run(SpringGrpcClientSecurityCsrfApplication.class, args);
 	}
 
+	@Bean
+	SimpleGrpc.SimpleBlockingStub simpleStub(GrpcChannelFactory channelFactory) {
+		return SimpleGrpc.newBlockingStub(channelFactory.createChannel("simple"));
+	}
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+				.httpBasic(withDefaults())
+				.csrf(CsrfConfigurer::disable)
+				.build();
+	}
+
+	@Bean
+	UserDetailsService userDetailsService() {
+		return new InMemoryUserDetailsManager(
+				User.withDefaultPasswordEncoder()
+						.username("user")
+						.password("password")
+						.build()
+		);
+	}
 }
