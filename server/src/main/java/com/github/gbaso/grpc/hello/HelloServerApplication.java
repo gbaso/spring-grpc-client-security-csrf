39263249c1207523de41a4458aa2dc9dd154f465
Giacomo Baso <gbaso@users.noreply.github.com>
2025-03-17 17:55:52 +0100

    add server application

---
+++ server/src/main/java/com/github/gbaso/grpc/hello/HelloServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.grpc.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloServerApplication.class, args);
+  }
+
+}
---
+++ server/src/test/java/com/github/gbaso/grpc/hello/HelloServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gbaso.grpc.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
